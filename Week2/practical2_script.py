55957b7fb592c591c3e6c2cfc7fa6cb9116e2688
Bob McNabb <robertmcnabb@gmail.com>
2025-03-11 21:28:20 +0000

    update example script to match notebook

--- Week2/practical2_script.py
+++ Week2/practical2_script.py
@@ -7,32 +7,73 @@ import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 
 
-# generate matplotlib handles to create a legend of each of the features we put in our map.
 def generate_handles(labels, colors, edge='k', alpha=1):
+    """
+    Generate matplotlib patch handles to create a legend of each of the features in the map.
+
+    Parameters
+    ----------
+
+    labels : list(str)
+        the text labels of the features to add to the legend
+
+    colors : list(matplotlib color)
+        the colors used for each of the features included in the map.
+
+    edge : matplotlib color (default: 'k')
+        the color to use for the edge of the legend patches.
+
+    alpha : float (default: 1.0)
+        the alpha value to use for the legend patches.
+
+    Returns
+    -------
+
+    handles : list(matplotlib.patches.Rectangle)
+        the list of legend patches to pass to ax.legend()
+    """
     lc = len(colors)  # get the length of the color list
     handles = [] # create an empty list
     for ii in range(len(labels)): # for each label and color pair that we're given, make an empty box to pass to our legend
         handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[ii % lc], edgecolor=edge, alpha=alpha))
     return handles
 
-# create a scale bar of length 20 km in the upper right corner of the map
 # adapted this question: https://stackoverflow.com/q/32333870
 # answered by SO user Siyh: https://stackoverflow.com/a/35705477
-def scale_bar(ax, location=(0.92, 0.95)):
+def scale_bar(ax, length=20, location=(0.92, 0.95)):
+    """
+    Create a scale bar in a cartopy GeoAxes.
+
+    Parameters
+    ----------
+
+    ax : cartopy.mpl.geoaxes.GeoAxes
+        the cartopy GeoAxes to add the scalebar to.
+
+    length : int, float (default 20)
+        the length of the scalebar, in km
+
+    location : tuple(float, float) (default (0.92, 0.95))
+        the location of the center right corner of the scalebar, in fractions of the axis.
+
+    Returns
+    -------
+    ax : cartopy.mpl.geoaxes.GeoAxes
+        the cartopy GeoAxes object
+
+    """
     x0, x1, y0, y1 = ax.get_extent() # get the current extent of the axis
-    sbx = x0 + (x1 - x0) * location[0] # get the lower left x coordinate of the scale bar
-    sby = y0 + (y1 - y0) * location[1] # get the lower left y coordinate of the scale bar
+    sbx = x0 + (x1 - x0) * location[0] # get the right x coordinate of the scale bar
+    sby = y0 + (y1 - y0) * location[1] # get the right y coordinate of the scale bar
 
-    ax.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=ax.projection) # plot a thick black line, 20 km long
-    ax.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=ax.projection) # plot a smaller black line from 0 to 10 km long
-    ax.plot([sbx-10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=ax.projection) # plot a white line from 10 to 20 km
+    ax.plot([sbx, sbx-length*1000], [sby, sby], color='k', linewidth=4, transform=ax.projection) # plot a thick black line
+    ax.plot([sbx-(length/2)*1000, sbx-length*1000], [sby, sby], color='w', linewidth=2, transform=ax.projection) # plot a white line from 0 to halfway
 
-    ax.text(sbx, sby-5000, '20 km', transform=ax.projection, fontsize=8) # add a label at 20 km
-    ax.text(sbx-12500, sby-5000, '10 km', transform=ax.projection, fontsize=8) # add a label at 10 km
-    ax.text(sbx-24500, sby-5000, '0 km', transform=ax.projection, fontsize=8) # add a label at 0 km
+    ax.text(sbx, sby-(length/4)*1000, f"{length} km", ha='center', transform=ax.projection, fontsize=6) # add a label at the right side
+    ax.text(sbx-(length/2)*1000, sby-(length/4)*1000, f"{int(length/2)} km", ha='center', transform=ax.projection, fontsize=6) # add a label in the center
+    ax.text(sbx-length*1000, sby-(length/4)*1000, '0 km', ha='center', transform=ax.projection, fontsize=6) # add a label at the left side
 
     return ax
-
 
 # load the datasets
 outline = gpd.read_file(os.path.abspath('data_files/NI_outline.shp'))
@@ -44,7 +85,7 @@ counties = gpd.read_file(os.path.abspath('data_files/Counties.shp'))
 ni_utm = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
 # NI is in UTM Zone 29, so we pass 29 to ccrs.UTM()
 
-myFig = plt.figure(figsize=(8, 8))  # create a figure of size 8x8 (representing the page size in inches)
+fig = plt.figure(figsize=(8, 8))  # create a figure of size 8x8 (representing the page size in inches)
 ax = plt.axes(projection=ni_utm)  # create an axes object in the figure, using a UTM projection,
 # where we can actually plot our data.
 
@@ -59,7 +100,7 @@ ax.set_extent([xmin-5000, xmax+5000, ymin-5000, ymax+5000], crs=ni_utm)  # becau
 # but set_extent takes xmin, xmax, ymin, ymax, we re-order the coordinates here.
 
 # pick colors, add features to the map
-county_colors = ['firebrick', 'seagreen', 'royalblue', 'coral', 'violet', 'cornsilk']
+county_colors = ['#003f5c', '#444e86', '#955196', '#dd5182', '#ff6e54', '#ffa600']
 
 # get a list of unique names for the county boundaries
 county_names = list(counties.CountyName.unique())
@@ -75,7 +116,7 @@ for ii, name in enumerate(county_names):
                           edgecolor='k', # outline the feature in black
                           facecolor=county_colors[ii], # set the face color to the corresponding color from the list
                           linewidth=1, # set the outline width to be 1 pt
-                          alpha=0.25) # set the alpha (transparency) to be 0.25 (out of 1)
+                          alpha=0.8) # set the alpha (transparency) to be 0.4 (out of 1)
     ax.add_feature(feat) # once we have created the feature, we have to add it to the map using ax.add_feature()
 
 # here, we're setting the edge color to be the same as the face color. Feel free to change this around,
@@ -99,7 +140,7 @@ town_handle = ax.plot(towns.geometry.x, towns.geometry.y, 's', color='0.5', ms=6
 # generate a list of handles for the county datasets
 # first, we add the list of names, then the list of colors, and finally we set the transparency
 # (since we set it in the map)
-county_handles = generate_handles(counties.CountyName.unique(), county_colors, alpha=0.25)
+county_handles = generate_handles(counties.CountyName.unique(), county_colors, alpha=0.8)
 
 # note: if you change the color you use to display lakes, you'll want to change it here, too
 water_handle = generate_handles(['Lakes'], ['mediumblue'])
@@ -133,4 +174,4 @@ for ind, row in towns.iterrows(): # towns.iterrows() returns the index and row
 scale_bar(ax)
 
 # save the figure as map.png, cropped to the axis (bbox_inches='tight'), and a dpi of 300
-myFig.savefig('map.png', bbox_inches='tight', dpi=300)
+fig.savefig('map.png', bbox_inches='tight', dpi=300)
