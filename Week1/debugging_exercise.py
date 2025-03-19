c0940080422a7050f3652d246b753318bdb3f324
tenerk <tener-k@ulster.ac.uk>
2025-03-19 15:02:21 +0000

    fixing guessing game script
    - correct random integer generation
    - fix user feedback
    -make sure that the input is cast as an integer

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -1,16 +1,16 @@
 import random
 
 # pick a random number for the user to guess
-rand = random.randint(1, 100)
+rand = random.randint(1, 20)
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
     if guess > rand:  # if the guess is too high, tell the user.
-        print('Too low. Guess again.')
-    else:  # if the guess is too low, tell the user.
         print('Too high. Guess again.')
+    else:  # if the guess is too low, tell the user.
+        print('Too low. Guess again.')
 
     print('Enter a new guess: ')
     guess = int(input())
