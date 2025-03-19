80f71d857f15d9d51ed05035ba1396b39dd4957f
tenerk <tener-k@ulster.ac.uk>
2025-03-19 15:23:14 +0000

    guess tracker added

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -2,11 +2,13 @@ import random
 
 # pick a random number for the user to guess
 rand = random.randint(1, 20)
+guess_count = 0
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
 
-while guess != rand:  # if the guess is not equal to the random number, you have to guess again
+while guess != rand: # if the guess is not equal to the random number, you have to guess again
+    guess_count += 1
     if guess > rand:  # if the guess is too high, tell the user.
         print('Too high. Guess again.')
     else:  # if the guess is too low, tell the user.
@@ -16,3 +18,4 @@ while guess != rand:  # if the guess is not equal to the random number, you have
     guess = int(input())
 
 print('You got it! The number was {}'.format(rand))
+print(f'You used {guess_count} guesses')
